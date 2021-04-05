81f0d6492d2f30022b230a4809a740a38a8b64bf
Yasser-1999 <81980981+Yasser-1999@users.noreply.github.com>
2021-04-05 23:49:24 +0700

    jam.h

---
+++ .gitignore/jam.h
@@ -0,0 +1,21 @@
+#include "jam.h"
+
+Int main() {
+  tJam jam;
+
+  jam.setJam(12,45,10);
+  jam.cetakJam();
+
+  /*
+  cout << "Jam:";
+  cin >> "jam.hh;
+  cout >> "Menit:";
+  cin >> jam.mm;
+  coit << "Detik:";
+  cin >> jam.ss;
+  */
+
+  //cout << "Jam sekarang "<< jam.hh << ":" << jam.mm << ":" << jam.ss << endl;
+  //cout << "Hello World!" << endl;
+  return 0;
+}
