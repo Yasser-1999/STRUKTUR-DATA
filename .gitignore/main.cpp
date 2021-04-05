a43d59ab752f09088c399ffff0fcf1fcceddae88
Yasser-1999 <81980981+Yasser-1999@users.noreply.github.com>
2021-04-05 23:41:25 +0700

    Main.cpp

---
+++ .gitignore/main.cpp
@@ -0,0 +1,17 @@
+#ifndef JAM_H_INCLUDED
+#define JAM_H_INCLUDED
+
+#include <iostream>
+using namespace std;
+
+Struct tJam {
+  int hh;
+  int mm;
+  int ss;
+ }
+  void cetakJam(){
+  Coun << "Jam sekarang "<< hh << ":" << mm << ":" << ss << endl;
+  }
+};
+
+#endif //JAM_H_INCLUDED
